e364624bc6ce28883f27027bf8c3af48f7850ebc
Sandro <sandrocarioli43@gmail.com>
2023-04-20 12:43:09 -0300

    wip cart

--- src/controllers/controller.cart.js
+++ src/controllers/controller.cart.js
@@ -13,8 +13,56 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
     Carts.create(req.body)
     res.json({ message: "cart created" })
 })
 
+router.get('/:cid', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}) 
+
+router.put('/:cid', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+router.post('/:cid/product/p:pid', async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+router.put('/:cid/product/:pid', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+router.delete('/:cid', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+router.delete('/:cid/product/:pid', async (req, res) => {
+
+})
 module.exports = router
--- src/controllers/controller.products.js
+++ src/controllers/controller.products.js
@@ -11,32 +11,59 @@ const FileManager = new fileManager;
 const ProDao = new ProductsDao;
 
 router.get('/', async (req, res) => {
-    const limit = req.query.limit ? parseInt(req.query.limit) : 10;
-    const page = req.query.page ? parseInt(req.query.page) : 1; 
-    const sort = req.query.sort || '-createdAt';
+    const limit = parseInt(req.query.limit) || 10;
+    const page = parseInt(req.query.page) || 1;
+    const sort = req.query.sort || '';
     const query = req.query.query || '';
+    const bystock = req.query.stock || '';
 
     try {
-        const product = await ProDao.findAll()
-        res.json({message: product})
-    } catch (error) {
-        res.json({ message: error})
+        const pipeline = [
+            { $unwind: '$products' },
+            { $match: { 'products.name': { $regex: query, $options: 'i' } } },
+            { $sort: { [sort]: -1 } },
+            { $sort: { stock: bystock === 'true' ? -1 : 1 } },
+            { $skip: (page - 1) * limit },
+            { $limit: limit }
+        ];
+
+        const result = await Products.aggregate(pipeline);
+
+        const countPipeline = [
+            { $unwind: '$products' },
+            { $match: { 'products.name': { $regex: query, $options: 'i' } } },
+            { $group: { _id: null, count: { $sum: 1 } } }
+        ];
+
+        const countResult = await Products.aggregate(countPipeline);
+        const count = countResult.length > 0 ? countResult[0].count : 0;
+
+        const totalPages = Math.ceil(count / limit);
+        const hasPrevPage = page > 1;
+        const hasNextPage = page < totalPages;
+        const prevLink = hasPrevPage ? `${req.protocol}://${req.get('host')}${req.baseUrl}?limit=${limit}&page=${page - 1}&sort=${sort}&query=${query}&stock=${bystock}` : null;
+        const nextLink = hasNextPage ? `${req.protocol}://${req.get('host')}${req.baseUrl}?limit=${limit}&page=${page + 1}&sort=${sort}&query=${query}&stock=${bystock}` : null;
+
+        res.status(200).json({
+            status: 'success',
+            payload: result,
+            totalPages,
+            prevPage: hasPrevPage ? page - 1 : null,
+            nextPage: hasNextPage ? page + 1 : null,
+            page,
+            hasPrevPage,
+            hasNextPage,
+            prevLink,
+            nextLink
+        });
+    } catch (err) {
+        res.status(500).json({ error: err.message, status: 'error' });
     }
 });
 
-router.get ('/loadItems', async (req, res) =>{
-    try {
-        const products = await FileManager.loadItems()
-        const newProduct = await ProDao.createMany(products)
-            res.json({message: newProduct})
-    } catch (error) {
-        res.json(error)
-    }
-})
-
 router.post('/', uploader.single('file'), async (req, res) => {
     try {
-        const {title, description, price, thumbnail, code, stock} = req.body
+        const { title, description, price, thumbnail, code, stock } = req.body
         const newProductInfo = {
             title,
             description,
@@ -47,18 +74,79 @@ router.post('/', uploader.single('file'), async (req, res) => {
             image: req.file.filename
         }
         const newProduct = await Products.create(newProductInfo)
-        res.json({message: newProduct})
+        res.json({ message: newProduct })
     } catch (error) {
-        if (error.code === 11000){
-            res.status(400).json({ error: "Este producto ya esta agregado en la base de datos."})
+        if (error.code === 11000) {
+            res.status(400).json({ error: "Este producto ya esta agregado en la base de datos." })
         }
         console.log(error)
-        res.status(500).json({ error: "Internal server error"})
+        res.status(500).json({ error: "Internal server error" })
     }
 })
 
-router.delete("/deleteAll", async (req, res) =>{
+router.get('/:id', async (req, res) => {
+    try {
+        const product = await Products.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+
+        res.status(200).json(product);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const product = await Products.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        product.name = req.body.name;
+        product.description = req.body.description;
+        product.price = req.body.price;
+        product.stock = req.body.stock;
+
+        const updatedProduct = await product.save();
+
+        res.status(200).json(updatedProduct);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const product = await Products.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+
+        await product.remove();
+
+        res.status(204).send();
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+router.get('/loadItems', async (req, res) => {
+    try {
+        const products = await FileManager.loadItems()
+        const newProduct = await ProDao.createMany(products)
+        res.json({ message: newProduct })
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+router.delete("/deleteAll", async (req, res) => {
     await ProDao.deleteAll()
-    res.json({message: "Doomed"})
-} )
+    res.json({ message: "Doomed" })
+})
 module.exports = router
