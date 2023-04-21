a15c1bee60887ca8451645eaf2afd81791d5ce8a
Sandro <sandrocarioli43@gmail.com>
2023-04-21 17:32:44 -0300

    search funcionando

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const { Server } = require("socket.io")
 const app = require("./app")
-const {port} = require("./config/app.config")
+const { port } = require("./config/app.config")
 
-const httpServer = app.listen(port, () =>{
+const httpServer = app.listen(port, () => {
     console.log(`server running at port ${port}`)
 })
 
 const io = new Server(httpServer)
 
-io.on('connection', socket =>{
+io.on('connection', socket => {
     console.log(`Client connected with id: ${socket.id}`)
     io.emit('mensajeServidor', 'Hola desde el servidor!!!')
-})
+    socket.on('busca', query => {
+        io.emit('actualizar', query);
+    });
+});
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -2,4 +2,18 @@ const socket = io()
 
 socket.on('mensajeServidor', message => {
     console.log(message)
-  })
+})
+
+const searchInput = document.getElementById("search-navbar")
+
+searchInput.addEventListener('keyup', (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        const query = searchInput.value;
+        socket.emit('busca', query);
+    }
+});
+
+socket.on('actualizar', query => {
+    location.href = `?query=${query}`;
+});
